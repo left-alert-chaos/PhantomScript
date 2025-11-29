bfd3b283f039e54deb4af89eeb2293c43bf61296
left-alert-chaos <236125075+left-alert-chaos@users.noreply.github.com>
2025-11-28 19:10:15 -0500

    updates'

--- main.py
+++ main.py
@@ -322,6 +322,8 @@ def error(errType):
     #traceback
     for i in layers:
         if isinstance(i, int):
+            if text[i+1].strip().split()[0] == "while":
+                i += 1
             print(f"Line {i + 1}:")
             print(f"{text[i].strip()}\n")
     print(f"Line {lineNum + 1}:")
